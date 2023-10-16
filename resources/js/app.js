7643ebe03f3409fcf6f9e0dbbd856d8e011da864
Kristian <kristiannrg@gmail.com>
2023-10-16 11:45:15 +0200

    project create

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp } from "vue";
+import * as VueRouter from "vue-router";
+
+import App from "./components/App.vue";
+import Welcome from "./pages/Welcome.vue";
+import Quiz from "./pages/Quiz.vue";
+
+const routes = [{ path: "/", component: Welcome }];
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
+});
+
+const app = createApp(App);
+app.use(router);
+
+app.mount("#app");
